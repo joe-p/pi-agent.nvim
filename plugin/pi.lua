b4b54355cc1f8d38868e2b855a8acf44635783b7
Joe Polny <joepolny@gmail.com>
2026-04-25 18:38:47 -0400

    never modified buf

--- plugin/pi.lua
+++ plugin/pi.lua
@@ -31,6 +31,19 @@ if not _G.pi_loaded then
     end,
   })
 
+  -- Prevent pi buffers from blocking :q with "no write since last change"
+  if vim.fn.exists('##BufModifiedSet') == 1 then
+    vim.api.nvim_create_autocmd('BufModifiedSet', {
+      group = vim.api.nvim_create_augroup('PiNoModified', { clear = true }),
+      callback = function(args)
+        local ft = vim.bo[args.buf].filetype
+        if ft == 'pichat' or ft == 'piinput' then
+          vim.bo[args.buf].modified = false
+        end
+      end,
+    })
+  end
+
   -- Create highlight groups
   vim.api.nvim_set_hl(0, 'PiUser', { fg = '#7aa2f7', bold = true })
   vim.api.nvim_set_hl(0, 'PiAssistant', { fg = '#9ece6a', bold = true })
